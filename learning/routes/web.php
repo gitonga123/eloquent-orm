d458476018b80e64a2561aaf7af36a5e176d92f9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-11 14:12:27 +0300

    Using Raw SQL

--- learning/routes/web.php
+++ learning/routes/web.php
@@ -18,5 +18,13 @@ Route::get('/', function () {
 //using the fluent builder.
 Route::get('/contact-list', function () {
     $contact = DB::table('contacts')->get();
+    echo "<pre>";
     var_dump($contact);
+    echo "</pre>";
+});
+
+//using raw Sql
+Route::get('/contact_list', function () {
+    $users = DB::select('select * from contacts');
+    dd($users);
 });
