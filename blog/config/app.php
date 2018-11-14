d3594d78c3e846282bcc7d0f525b95c8167fbb0a
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-14 17:10:16 +0300

    Elastic Search using - Elasticquent

--- blog/config/app.php
+++ blog/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Elasticquent\ElasticquentServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -224,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Es' => Elasticquent\ElasticquentElasticsearchFacade::class,
 
     ],
 
