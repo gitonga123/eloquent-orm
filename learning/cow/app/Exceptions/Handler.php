7d434e679703a14736297d8e4511b113edb62d81
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-22 07:44:09 +0300

    Render an exception into an HTTP response for json request :fire:

--- learning/cow/app/Exceptions/Handler.php
+++ learning/cow/app/Exceptions/Handler.php
@@ -12,9 +12,7 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -48,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
         return parent::render($request, $exception);
     }
 }
